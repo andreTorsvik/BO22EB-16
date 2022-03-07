ccf067d78d3ac97785211dbda780a960875dbcff
Ole Sunde <585120@stud.hvl.no>
2022-03-07 12:31:23 +0100

    Lagt til komponeter i "Legg til Område"

--- GMAP Demo/Forms/frm_R_LeggTilOmråde.Designer.cs
+++ GMAP Demo/Forms/frm_R_LeggTilOmråde.Designer.cs
@@ -29,6 +29,31 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKommentar = new System.Windows.Forms.TextBox();
+            this.btnLeggTilOverlay = new System.Windows.Forms.Button();
+            this.lbValgt = new System.Windows.Forms.ListBox();
+            this.lbTilgjengelige = new System.Windows.Forms.ListBox();
+            this.btnLeggTilObjekt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSikkerhetsklarering = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNavn = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbPunkter = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtfarge = new System.Windows.Forms.TextBox();
+            this.lbTilgjengligFarge = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label3
@@ -42,17 +67,277 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Legg til Område";
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label11.Location = new System.Drawing.Point(167, 506);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(139, 16);
+            this.label11.TabIndex = 50;
+            this.label11.Text = "Valgt for dette område";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label10.Location = new System.Drawing.Point(20, 506);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 16);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "Ekisterende";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label9.Location = new System.Drawing.Point(20, 724);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 20);
+            this.label9.TabIndex = 48;
+            this.label9.Text = "Ny Overlay ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.Location = new System.Drawing.Point(20, 751);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 22);
+            this.textBox2.TabIndex = 47;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label6.Location = new System.Drawing.Point(20, 481);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(180, 20);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "Velg Hvilke overlays";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label7.Location = new System.Drawing.Point(20, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "Long";
+            // 
+            // txtLong
+            // 
+            this.txtLong.BackColor = System.Drawing.SystemColors.Control;
+            this.txtLong.Location = new System.Drawing.Point(20, 321);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(285, 22);
+            this.txtLong.TabIndex = 44;
+            this.txtLong.Text = "klikk på kartet ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label8.Location = new System.Drawing.Point(20, 256);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 20);
+            this.label8.TabIndex = 43;
+            this.label8.Text = "Lat";
+            // 
+            // txtLat
+            // 
+            this.txtLat.BackColor = System.Drawing.SystemColors.Control;
+            this.txtLat.Location = new System.Drawing.Point(20, 277);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(287, 22);
+            this.txtLat.TabIndex = 42;
+            this.txtLat.Text = "klikk på kartet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label5.Location = new System.Drawing.Point(20, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 20);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Kommentar";
+            // 
+            // txtKommentar
+            // 
+            this.txtKommentar.Location = new System.Drawing.Point(20, 189);
+            this.txtKommentar.Name = "txtKommentar";
+            this.txtKommentar.Size = new System.Drawing.Size(285, 22);
+            this.txtKommentar.TabIndex = 40;
+            // 
+            // btnLeggTilOverlay
+            // 
+            this.btnLeggTilOverlay.Location = new System.Drawing.Point(148, 749);
+            this.btnLeggTilOverlay.Name = "btnLeggTilOverlay";
+            this.btnLeggTilOverlay.Size = new System.Drawing.Size(73, 27);
+            this.btnLeggTilOverlay.TabIndex = 39;
+            this.btnLeggTilOverlay.Text = "Legg til";
+            this.btnLeggTilOverlay.UseVisualStyleBackColor = true;
+            // 
+            // lbValgt
+            // 
+            this.lbValgt.FormattingEnabled = true;
+            this.lbValgt.ItemHeight = 16;
+            this.lbValgt.Location = new System.Drawing.Point(167, 525);
+            this.lbValgt.Name = "lbValgt";
+            this.lbValgt.Size = new System.Drawing.Size(140, 196);
+            this.lbValgt.TabIndex = 38;
+            // 
+            // lbTilgjengelige
+            // 
+            this.lbTilgjengelige.FormattingEnabled = true;
+            this.lbTilgjengelige.ItemHeight = 16;
+            this.lbTilgjengelige.Location = new System.Drawing.Point(20, 525);
+            this.lbTilgjengelige.Name = "lbTilgjengelige";
+            this.lbTilgjengelige.Size = new System.Drawing.Size(141, 196);
+            this.lbTilgjengelige.TabIndex = 37;
+            // 
+            // btnLeggTilObjekt
+            // 
+            this.btnLeggTilObjekt.Location = new System.Drawing.Point(20, 779);
+            this.btnLeggTilObjekt.Name = "btnLeggTilObjekt";
+            this.btnLeggTilObjekt.Size = new System.Drawing.Size(123, 34);
+            this.btnLeggTilObjekt.TabIndex = 36;
+            this.btnLeggTilObjekt.Text = "Lagre området";
+            this.btnLeggTilObjekt.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label4.Location = new System.Drawing.Point(20, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 20);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Sikkerhetsklarering(1-3)";
+            // 
+            // txtSikkerhetsklarering
+            // 
+            this.txtSikkerhetsklarering.Location = new System.Drawing.Point(20, 145);
+            this.txtSikkerhetsklarering.Name = "txtSikkerhetsklarering";
+            this.txtSikkerhetsklarering.Size = new System.Drawing.Size(287, 22);
+            this.txtSikkerhetsklarering.TabIndex = 34;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label1.Location = new System.Drawing.Point(20, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Navn";
+            // 
+            // txtNavn
+            // 
+            this.txtNavn.Location = new System.Drawing.Point(20, 101);
+            this.txtNavn.Name = "txtNavn";
+            this.txtNavn.Size = new System.Drawing.Size(285, 22);
+            this.txtNavn.TabIndex = 32;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 28);
+            this.button1.TabIndex = 51;
+            this.button1.Text = "Legg til punkt";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbPunkter
+            // 
+            this.lbPunkter.FormattingEnabled = true;
+            this.lbPunkter.ItemHeight = 16;
+            this.lbPunkter.Location = new System.Drawing.Point(20, 383);
+            this.lbPunkter.Name = "lbPunkter";
+            this.lbPunkter.Size = new System.Drawing.Size(285, 84);
+            this.lbPunkter.TabIndex = 52;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 352);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 22);
+            this.textBox1.TabIndex = 53;
+            this.textBox1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label2.Location = new System.Drawing.Point(20, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 55;
+            this.label2.Text = "Farge";
+            // 
+            // txtfarge
+            // 
+            this.txtfarge.Location = new System.Drawing.Point(167, 233);
+            this.txtfarge.Name = "txtfarge";
+            this.txtfarge.ReadOnly = true;
+            this.txtfarge.Size = new System.Drawing.Size(138, 22);
+            this.txtfarge.TabIndex = 54;
+            // 
+            // lbTilgjengligFarge
+            // 
+            this.lbTilgjengligFarge.FormattingEnabled = true;
+            this.lbTilgjengligFarge.ItemHeight = 16;
+            this.lbTilgjengligFarge.Location = new System.Drawing.Point(20, 233);
+            this.lbTilgjengligFarge.Name = "lbTilgjengligFarge";
+            this.lbTilgjengligFarge.Size = new System.Drawing.Size(138, 20);
+            this.lbTilgjengligFarge.TabIndex = 56;
+            // 
             // frm_R_LeggTilOmråde
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(391, 825);
+            this.Controls.Add(this.lbTilgjengligFarge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtfarge);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbPunkter);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtLat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtKommentar);
+            this.Controls.Add(this.btnLeggTilOverlay);
+            this.Controls.Add(this.lbValgt);
+            this.Controls.Add(this.lbTilgjengelige);
+            this.Controls.Add(this.btnLeggTilObjekt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSikkerhetsklarering);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNavn);
             this.Controls.Add(this.label3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_R_LeggTilOmråde";
             this.Text = "frm_R_LeggTilOmråde";
-            this.Load += new System.EventHandler(this.frm_R_LeggTilOmråde_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,5 +346,30 @@
         #endregion
 
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKommentar;
+        private System.Windows.Forms.Button btnLeggTilOverlay;
+        private System.Windows.Forms.ListBox lbValgt;
+        private System.Windows.Forms.ListBox lbTilgjengelige;
+        private System.Windows.Forms.Button btnLeggTilObjekt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSikkerhetsklarering;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNavn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbPunkter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtfarge;
+        private System.Windows.Forms.ListBox lbTilgjengligFarge;
     }
 }
