77795da251f48714b731d4b9edbe3035b64b517b
Andre Haldorsen Torsvik <583662@stud.hvl.no>
2022-03-15 11:32:56 +0100

    Flyttet tilhørende kommentarer til Bildebehandling klasse.

--- GMAP Demo/Bildebehandling/Bildebehandling.cs
+++ GMAP Demo/Bildebehandling/Bildebehandling.cs
@@ -10,7 +10,7 @@ namespace GMAP_Demo
 {
     internal class Bildebehandling
     {
-
+        // Metoder hentet fra: //https://www.codeproject.com/Questions/872267/store-image-in-a-variable-in-csharp-Please-Help-me
         public Image byteArrayToImage(byte[] byteArrayIn)
         {
             MemoryStream ms = new MemoryStream(byteArrayIn);
