a86fb040d50366e8824c539d0c62704fe432da8a
Andre Haldorsen Torsvik <583662@stud.hvl.no>
2022-03-15 11:31:26 +0100

    Flyttet tilhørende metoder til bildebehandling klasse.

--- GMAP Demo/Bildebehandling/Bildebehandling.cs
+++ GMAP Demo/Bildebehandling/Bildebehandling.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GMAP_Demo.Bildebehandling
+namespace GMAP_Demo
 {
     internal class Bildebehandling
     {
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
+        }
 
 
     }
