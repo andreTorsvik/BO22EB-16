f8a0e937ec89aca02dafaf2b6164d5155e63a552
Andre Haldorsen Torsvik <583662@stud.hvl.no>
2022-04-05 08:32:38 +0200

    Begynt på metode for å opprette ommråde og sende til database.

--- GMAP Demo/Forms/Redigering/frm_R_LeggTilOmråde.cs
+++ GMAP Demo/Forms/Redigering/frm_R_LeggTilOmråde.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
 
 namespace GMAP_Demo
 {
@@ -17,13 +18,30 @@ namespace GMAP_Demo
         {
             InitializeComponent();
             instance = this;
-            
+        }
+        // Opprette liste av punkter for område
+        BindingList<PointLatLng> pointLatLngs;
+        private void InitializepointLatLngs()
+        {
+            // Create the new BindingList of Part type.
+            pointLatLngs = new BindingList<PointLatLng>();
+
+            // Allow new parts to be added, but not removed once committed.        
+            pointLatLngs.AllowNew = true;
+            pointLatLngs.AllowRemove = true;
+
+            // Raise ListChanged events when new parts are added.
+            pointLatLngs.RaiseListChangedEvents = true;
         }
 
         private void frm_R_LeggTilOmråde_Load(object sender, EventArgs e)
         {
             LastInnOverlays();
             LastInnFargerMuligheter();
+
+            InitializepointLatLngs();
+            lbPunkter.DataSource = pointLatLngs;
+            //lbPunkter.DisplayMember = "Kategorinavn";
         }
 
         private void LastInnOverlays()
@@ -37,7 +55,7 @@ namespace GMAP_Demo
             {
                 AlleOverlay.Add(item.Kategori.ToString());
             }
-            
+
             var OverlayRListe = db.ListAllOverlay_RessursFromDb();
             foreach (var item in OverlayRListe)
             {
@@ -106,6 +124,80 @@ namespace GMAP_Demo
         {
 
         }
+
+        private void btnLeggTilOmrådeIDb_Click(object sender, EventArgs e)
+        {
+            bool altUtfylt = true;
+            string utFyllingsmangler = "Du mangler:";
+            //kode for sjekk at alle felten er utfylt
+            if (string.IsNullOrWhiteSpace(txtNavn.Text))
+            {
+                altUtfylt = false;
+                utFyllingsmangler += " Navn";
+            }
+            if (string.IsNullOrWhiteSpace(txtSikkerhetsklarering.Text))
+            {
+                altUtfylt = false;
+                utFyllingsmangler += " Sikkerhetsklarering";
+            }
+            if (string.IsNullOrWhiteSpace(txtKommentar.Text))
+            {
+                altUtfylt = false;
+                utFyllingsmangler += " Kommentar";
+            }
+            if (string.IsNullOrWhiteSpace(txtfarge.Text))
+            {
+                altUtfylt = false;
+                utFyllingsmangler += " Farge";
+            }
+            if (pointLatLngs.Count > 2)
+            {
+                altUtfylt = false;
+                utFyllingsmangler += " Tilstrekkelige punkter (>2) til å tegne et område";
+            }
+            if (lbValgtOverlays.Items.Count > 0)
+            {
+                altUtfylt = false;
+                utFyllingsmangler += " Overlay";
+            }
+
+            if (altUtfylt)
+            {
+                DatabaseCommunication db = new DatabaseCommunication();
+
+                // må tilpasses område
+
+                // må opprette metode for område
+
+
+                db.InsertRessursToDb(txtNavn.Text.ToString(), txtfarge.Text.ToString(), InnloggetBruker.BrukernavnInnlogget, Convert.ToInt32(txtSikkerhetsklarering.Text), txtKommentar.Text.ToString(), Convert.ToSingle(txtLat.Text), Convert.ToSingle(txtLong.Text));
+
+                txtNavn.Text = "";
+                txtSikkerhetsklarering.Text = "";
+                txtKommentar.Text = "";
+                txtfarge.Text = "";
+                txtLat.Text = "";
+                txtLong.Text = "";
+            }
+            else MessageBox.Show(utFyllingsmangler);
+        }
+
+        private void btnLeggTilPunktIListe_Click(object sender, EventArgs e)
+        {
+            if ((txtLat != null) && (txtLong != null))
+            {
+                pointLatLngs.Add(new PointLatLng(Convert.ToDouble(txtLat.Text), Convert.ToDouble(txtLong.Text)));
+                
+                if (pointLatLngs.Count > 2)
+                {
+                    // tegn område
+                }
+            }
+        }
+
+        private void btnFjernPunktIListe_Click(object sender, EventArgs e)
+        {
+            pointLatLngs.Remove((PointLatLng)lbPunkter.SelectedItem);
+        }
     }
-    
 }
