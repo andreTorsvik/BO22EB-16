430740f86eaca5eef3713769e6d62b9ec43cf39b
Andre Haldorsen Torsvik <583662@stud.hvl.no>
2022-04-19 10:49:09 +0200

    Lagt inn logo i innloggingsvindu som projektresurs.

--- GMAP Demo/Properties/Resources.Designer.cs
+++ GMAP Demo/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace GMAP_Demo.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap HVL {
+            get {
+                object obj = ResourceManager.GetObject("HVL", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_person_64 {
             get {
                 object obj = ResourceManager.GetObject("icons8-person-64", resourceCulture);
