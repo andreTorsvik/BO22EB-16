747f9baa30697a0db598cd7e6a714a431442cdb1
Ole Sunde <585120@stud.hvl.no>
2022-04-24 17:24:09 +0200

    #

--- GMAP Demo/Forms/Innlogging/frmInnlogging.Designer.cs
+++ GMAP Demo/Forms/Innlogging/frmInnlogging.Designer.cs
@@ -40,7 +40,7 @@
             this.lblTitle2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.LbNyBruker = new System.Windows.Forms.Label();
-            this.btNyBruker = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -169,7 +169,7 @@
             this.LbNyBruker.AutoSize = true;
             this.LbNyBruker.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LbNyBruker.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(255)))));
-            this.LbNyBruker.Location = new System.Drawing.Point(71, 356);
+            this.LbNyBruker.Location = new System.Drawing.Point(172, 272);
             this.LbNyBruker.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LbNyBruker.Name = "LbNyBruker";
             this.LbNyBruker.Size = new System.Drawing.Size(106, 16);
@@ -179,20 +179,16 @@
             this.LbNyBruker.MouseEnter += new System.EventHandler(this.LbNyBruker_MouseEnter);
             this.LbNyBruker.MouseLeave += new System.EventHandler(this.LbNyBruker_MouseLeave);
             // 
-            // btNyBruker
+            // label1
             // 
-            this.btNyBruker.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(255)))));
-            this.btNyBruker.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btNyBruker.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btNyBruker.ForeColor = System.Drawing.Color.White;
-            this.btNyBruker.Location = new System.Drawing.Point(242, 344);
-            this.btNyBruker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btNyBruker.Name = "btNyBruker";
-            this.btNyBruker.Size = new System.Drawing.Size(111, 32);
-            this.btNyBruker.TabIndex = 6;
-            this.btNyBruker.Text = "Opprett bruker";
-            this.btNyBruker.UseVisualStyleBackColor = false;
-            this.btNyBruker.Click += new System.EventHandler(this.btNyBruker_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 8.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(71, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ingen Bruker?";
             // 
             // frmInnlogging
             // 
@@ -200,11 +196,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(470, 428);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.LbNyBruker);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblTitle2);
             this.Controls.Add(this.lblTest);
-            this.Controls.Add(this.btNyBruker);
             this.Controls.Add(this.btnTestUser);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbPassword);
@@ -240,6 +236,6 @@
         private System.Windows.Forms.Label lblTitle2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label LbNyBruker;
-        private System.Windows.Forms.Button btNyBruker;
+        private System.Windows.Forms.Label label1;
     }
 }
