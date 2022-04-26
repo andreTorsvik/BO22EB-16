074d5d40f6a6026a3886d93abaece28f34f2bfac
Ole Sunde <585120@stud.hvl.no>
2022-04-26 15:15:57 +0200

    satt inn range i MålingSim

--- GMAP Demo/MålingSim/MålingSim.cs
+++ GMAP Demo/MålingSim/MålingSim.cs
@@ -17,9 +17,12 @@ namespace GMAP_Demo
         public void TimerProc(object state)
         {
             // perform the operation
-            float måling = (Convert.ToSingle(random.Next()) + Convert.ToSingle(random.NextDouble()));
+            float måling = (Convert.ToSingle(random.Next(30)) + Convert.ToSingle(random.NextDouble()));
+            Math.Round(måling, 2);
             DBComMåling.UpdateMåling("Termomåler 9001", (måling), 0, "C");
-            måling = (Convert.ToSingle(random.Next()) + Convert.ToSingle(random.NextDouble()));
+            
+            måling = (Convert.ToSingle(random.Next(400)) + Convert.ToSingle(random.NextDouble()));
+            Math.Round(måling, 2);
             DBComMåling.UpdateMåling("Voltron 777", (måling), 1, "V");
         }
     }
