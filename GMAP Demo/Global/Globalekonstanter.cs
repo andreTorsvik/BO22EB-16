628452251dd83cede79423263efcd41e66fb25fa
Andre Haldorsen Torsvik <583662@stud.hvl.no>
2022-04-29 13:48:13 +0200

    Kommentert og flyttet litt i globale konstanter

--- GMAP Demo/Global/Globalekonstanter.cs
+++ GMAP Demo/Global/Globalekonstanter.cs
@@ -9,16 +9,21 @@ namespace GMAP_Demo
 {
     public class Globalekonstanter
     {
+        // Sikkerhetsklarering (1-3)
         public static int MaxSikkerhetsklarering = 3;
-        public static Color knapp_trykket = Color.FromArgb(46, 51, 73);
-        public static Color StandarFargeKnapp = Color.FromArgb(24, 30, 54);
 
-
+        // Default textbox text for input Lat+Long
         public const string tekstLatLong_område = "Dobbelklikk på kartet + \"legg til\"";
         public const string tekstLatLong_objekt = "Dobbelklikk på kartet";
 
         // Tilpassing av størrelse på viste ikoner på kart:
         public const double MaxWidth = 40.0; // Max bredde i pixler
         public const double MaxHeight = 70.0; // Max Høyde i pixler
+
+
+
+        // Design/Theme
+        public static Color knapp_trykket = Color.FromArgb(46, 51, 73);
+        public static Color StandarFargeKnapp = Color.FromArgb(24, 30, 54);
     }
 }
