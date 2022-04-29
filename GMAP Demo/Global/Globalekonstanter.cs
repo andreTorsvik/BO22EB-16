42cf992330f217af030775d8a05f609f47bdb35a
Andre Haldorsen Torsvik <583662@stud.hvl.no>
2022-04-29 10:57:23 +0200

    Flyttet konstanter for skalering av ikoner til globake konstanter.

--- GMAP Demo/Global/Globalekonstanter.cs
+++ GMAP Demo/Global/Globalekonstanter.cs
@@ -12,5 +12,10 @@ namespace GMAP_Demo
         public static int MaxSikkerhetsklarering = 3;
         public static Color knapp_trykket = Color.FromArgb(46, 51, 73);
         public static Color StandarFargeKnapp = Color.FromArgb(24, 30, 54);
+
+
+        // Tilpassing av størrelse på viste ikoner på kart:
+        public const double MaxWidth = 40.0; // Max bredde i pixler
+        public const double MaxHeight = 70.0; // Max Høyde i pixler
     }
 }
